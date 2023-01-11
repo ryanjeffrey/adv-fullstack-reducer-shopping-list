df93142816d7d9b9a0b0b53e0831f492997e86a3
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-01-11 16:59:20 -0600

    Update args for create function

--- src/services/shopping-list-items.js
+++ src/services/shopping-list-items.js
@@ -23,11 +23,12 @@ export async function getShoppingListItem(shoppingListItemId) {
   return checkError(response);
 }
 
-export async function createShoppingListItem(fields) {
+export async function createShoppingListItem(item, quantity) {
   const response = await client
     .from('shopping-list')
     .insert({
-      ...fields,
+      item,
+      quantity
     })
   ;
   return checkError(response);
