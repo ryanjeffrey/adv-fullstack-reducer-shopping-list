a7b43a4e374709ec67a8c6d6563d4359179e8757
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-01-11 17:00:11 -0600

    Import actions and new form for creating a new list item

--- src/components/Page/ShoppingListPage.jsx
+++ src/components/Page/ShoppingListPage.jsx
@@ -1,21 +1,45 @@
 import ShoppingListItems from '../ShoppingList/ShoppingListItems';
 import { useContext, useEffect } from 'react';
 import { Context } from '../ShoppingListProvider.jsx';
-import { getShoppingListItems } from '../../services/shopping-list-items';
-import { 
-  shoppingListLoadSuccessAction 
+import {
+  createShoppingListItem,
+} from '../../services/shopping-list-items';
+import {
+  shoppingListCandidateItemChanged,
+  shoppingListCandidateQuantityChanged
 } from '../../actions/shopping-list-actions.js';
+import ShoppingListItemForm from '../ShoppingList/ShoppingListItemForm';
+import { 
+  getShoppingListItemsEffect 
+} from '../../effects/shopping-list-effects';
 
 export default function ShoppingListPage() {
   const { state, dispatch } = useContext(Context);
   useEffect(() => {
-    (async () => {
-      const items = await getShoppingListItems();
-      dispatch(shoppingListLoadSuccessAction(items));
-    })();
+    getShoppingListItemsEffect(dispatch);
   }, []);
-  return <section>
-    <h1>My Shopping List</h1>
-    <ShoppingListItems shoppingList={state.shoppingList} />
-  </section>;
+  const onItemChanged = (item) => {
+    dispatch(shoppingListCandidateItemChanged(item));
+  };
+  const onQuantityChanged = (quantity) => {
+    dispatch(shoppingListCandidateQuantityChanged(quantity));
+  };
+  return (
+    <section>
+      <h1>My Shopping List</h1>
+      <ShoppingListItemForm
+        item={state.shoppingListCandidateItem}
+        onItemChanged={onItemChanged}
+        quantity={state.shoppingListCandidateQuantity}
+        onQuantityChanged={onQuantityChanged}
+        onSubmit={async (item, quantity) => {
+          await createShoppingListItem(item, quantity);
+          getShoppingListItemsEffect(dispatch);
+          dispatch(shoppingListCandidateItemChanged(''));
+          dispatch(shoppingListCandidateQuantityChanged(''));
+        }}
+      />
+      <ShoppingListItems shoppingList={state.shoppingList} />
+    </section>
+  );
 }
