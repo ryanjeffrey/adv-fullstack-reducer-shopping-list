d3e2654c3444e214948c5eedcf0203c46771498a
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-01-10 20:28:50 -0600

    Import shopping list items into shopping list page

--- src/components/Page/ShoppingListPage.jsx
+++ src/components/Page/ShoppingListPage.jsx
@@ -1,5 +1,21 @@
+import ShoppingListItems from '../ShoppingList/ShoppingListItems';
+import { useContext, useEffect } from 'react';
+import { Context } from '../ShoppingListProvider.jsx';
+import { getShoppingListItems } from '../../services/shopping-list-items';
+import { 
+  shoppingListLoadSuccessAction 
+} from '../../actions/shopping-list-actions.js';
+
 export default function ShoppingListPage() {
+  const { state, dispatch } = useContext(Context);
+  useEffect(() => {
+    (async () => {
+      const items = await getShoppingListItems();
+      dispatch(shoppingListLoadSuccessAction(items));
+    })();
+  }, []);
   return <section>
     <h1>My Shopping List</h1>
+    <ShoppingListItems shoppingList={state.shoppingList} />
   </section>;
 }
