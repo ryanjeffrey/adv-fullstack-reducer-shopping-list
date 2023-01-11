ace02137fef56ceedc1bc1f49342cdadc3a63bfe
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-01-11 16:57:46 -0600

    Add actions for creating a new item and quantity

--- src/actions/shopping-list-actions.js
+++ src/actions/shopping-list-actions.js
@@ -4,3 +4,17 @@ export const shoppingListLoadSuccessAction = (shoppingList) => {
     shoppingList,
   };
 };
+
+export const shoppingListCandidateItemChanged = (item) => {
+  return {
+    item,
+    type: 'shopping-list-candidate-item-changed',
+  };
+};
+
+export const shoppingListCandidateQuantityChanged = (quantity) => {
+  return {
+    quantity,
+    type: 'shopping-list-candidate-quantity-changed',
+  };
+};
