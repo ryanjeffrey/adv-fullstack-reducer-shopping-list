f1051e1a21cb315bc1e9d90b3f201f47c167f896
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-01-11 18:36:37 -0600

    Handle bought change in list items component

--- src/components/ShoppingList/ShoppingListItems.jsx
+++ src/components/ShoppingList/ShoppingListItems.jsx
@@ -1,12 +1,19 @@
 import ShoppingListItem from './ShoppingListItem.jsx';
 
-export default function ShoppingListItems({ shoppingList }) {
+export default function ShoppingListItems({ 
+  shoppingList, 
+  handleBoughtChangedByItemId,
+}) {
   return (
     <ul>
       {shoppingList.map((item) => {
         return (
           <li key={item.id}>
-            <ShoppingListItem item={item} />
+            <ShoppingListItem 
+              item={item} 
+              handleBoughtChanged={(bought) => {
+                handleBoughtChangedByItemId(item.id, bought);
+              }} />
           </li>
         );
       })}
